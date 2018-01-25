3aa1dff66318a48d19b6caba1d9d47e0ca54bf25
Dimitrii Kozlov <ikzdimitrij@gmail.com>
2018-01-25 13:42:26 +0200

    init

---
+++ NearestPlaces/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace NearestPlaces.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ NearestPlaces/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace NearestPlaces.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
